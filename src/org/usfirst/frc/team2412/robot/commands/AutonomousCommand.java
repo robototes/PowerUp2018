4c6560d3e83450ff390438305f7d338e4e3346c1
Robbie Robototes <robototes2412@gmail.com>
2018-01-16 16:12:56 -0800

    Added AutonomousCommand.

---
+++ src/org/usfirst/frc/team2412/robot/commands/AutonomousCommand.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team2412.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutonomousCommand extends CommandGroup {
+	public AutonomousCommand() {
+		addSequential(new DriveXFeetCommand());
+	}
+}
--- src/org/usfirst/frc/team2412/robot/commands/DriveXFeetCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/DriveXFeetCommand.java
@@ -16,6 +16,8 @@ import com.ctre.phoenix.motorcontrol.FeedbackDevice;
  * An example command.  You can replace me with your own command.
  */
 public class DriveXFeetCommand extends CommandBase {
+	private boolean firstRun = true;
+	
 	public DriveXFeetCommand() {
 		// Use requires() here to declare subsystem dependencies
 		requires(driveBase);
@@ -24,32 +26,44 @@ public class DriveXFeetCommand extends CommandBase {
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
+		RobotMap.talons[0].configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
 		RobotMap.talons[1].configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
 	}
 	
 	@Override
 	public void start() {
 		super.start();
-		RobotMap.talons[1].setSelectedSensorPosition(0, 0, 0);
+		System.out.println("Hello, world!");
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
-		System.out.println(RobotMap.talons[1].getSelectedSensorPosition(0));
+		System.out.println("First run: " + firstRun);
+		if(firstRun) {
+			System.out.println(firstRun);
+			RobotMap.talons[0].setSelectedSensorPosition(0, 0, 0);
+			RobotMap.talons[1].setSelectedSensorPosition(0, 0, 0);
+			firstRun = false;
+			System.out.println(Math.abs(RobotMap.talons[0].getSelectedSensorPosition(0)));
+			System.out.println(Math.abs(RobotMap.talons[1].getSelectedSensorPosition(0)));
+		}
+		System.out.println(Math.abs(RobotMap.talons[0].getSelectedSensorPosition(0)));
+		System.out.println(Math.abs(RobotMap.talons[1].getSelectedSensorPosition(0)));
 		driveBase.drive(0.5, 0, false);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return -RobotMap.talons[0].getSelectedSensorPosition(0) > 28000 && RobotMap.talons[1].getSelectedSensorPosition(0) > 28000;
+		return Math.abs(RobotMap.talons[0].getSelectedSensorPosition(0)) > 5000 && Math.abs(RobotMap.talons[1].getSelectedSensorPosition(0)) > 5000;
 //		return -RobotMap.talons[0].getSelectedSensorPosition(0) > 1000 && RobotMap.talons[1].getSelectedSensorPosition(0) > 1000;
 	}
 
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
+		firstRun = true;
 	}
 
 	// Called when another command which requires one or more of the same
