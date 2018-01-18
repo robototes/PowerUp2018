beefedfd8399c4cbfa704c1aebbdba0b3ce9cce3
Robbie Robototes <robototes2412@gmail.com>
2018-01-17 18:28:10 -0800

    Automatically detect which starting position we're at.

--- src/org/usfirst/frc/team2412/robot/PlateColorChecker.java
+++ src/org/usfirst/frc/team2412/robot/PlateColorChecker.java
@@ -1,9 +1,14 @@
 package org.usfirst.frc.team2412.robot;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class PlateColorChecker {
-
+	
+	private static NetworkTableInstance tableInstance = NetworkTableInstance.getDefault();
+	private static NetworkTable table = tableInstance.getTable("PyDashboard");
+	
 	public static boolean isLeftSwitchCorrectColor() {
 		return messageToCharArray()[0] == 'L';
 	}
@@ -24,4 +29,21 @@ public class PlateColorChecker {
 		return DriverStation.getInstance().getGameSpecificMessage().toCharArray(); 
 	}
 	
+	public static boolean isSwitchCorrectColor() {
+		String selected = table.getEntry("startingPosition").getString("unknown");
+		if(selected.equals("Left")) {
+			return isLeftSwitchCorrectColor();
+		} else {
+			return isRightSwitchCorrectColor();
+		}
+	}
+	
+	public static boolean isScaleCorrectColor() {
+		String selected = table.getEntry("startingPosition").getString("unknown");
+		if(selected.equals("Left")) {
+			return isLeftScaleCorrectColor();
+		} else {
+			return isRightScaleCorrectColor();
+		}
+	}
 }
