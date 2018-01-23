9302d5219c366210c0261ed0a2dc9fa5fe76028e
Robbie Robototes <robototes2412@gmail.com>
2018-01-22 19:45:01 -0800

    Added EncoderCommand class.

---
+++ src/org/usfirst/frc/team2412/robot/commands/EncoderCommand.java
@@ -0,0 +1,82 @@
+package org.usfirst.frc.team2412.robot.commands;
+
+import org.usfirst.frc.team2412.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
+public class EncoderCommand extends CommandBase {
+	private boolean firstRun = true;
+	private double startingValueLeft = 0;
+	private double startingValueRight = 0;
+	
+	private double distanceToDriveLeft;
+	private double distanceToDriveCenter;
+	private double distanceToDriveRight;
+	
+	private double distanceToDrive = 0;
+	
+	public EncoderCommand(double leftDistance, double centerDistance, double rightDistance) {
+		distanceToDriveLeft = leftDistance;
+		distanceToDriveCenter = centerDistance;
+		distanceToDriveRight = rightDistance;
+		// Use requires() here to declare subsystem dependencies
+		requires(driveBase);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		RobotMap.talons[0].configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+		RobotMap.talons[1].configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+	}
+	
+	@Override
+	public void start() {
+		super.start();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		if(exitEarly()) {
+			return;
+		}
+		if(firstRun) {
+			startingValueLeft = RobotMap.talons[0].getSelectedSensorPosition(0);
+			startingValueRight = RobotMap.talons[1].getSelectedSensorPosition(0);
+			firstRun = false;
+			distanceToDrive = getDistanceToDrive();
+		}
+		System.out.println(Math.abs(RobotMap.talons[0].getSelectedSensorPosition(0) - startingValueLeft));
+		System.out.println(Math.abs(RobotMap.talons[1].getSelectedSensorPosition(0) - startingValueRight));
+		driveBase.drive(0.5, 0, false);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return exitEarly() || Math.abs(RobotMap.talons[0].getSelectedSensorPosition(0) - startingValueLeft) > distanceToDrive && Math.abs(RobotMap.talons[1].getSelectedSensorPosition(0) - startingValueRight) > distanceToDrive;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		firstRun = true;
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+	
+	// If we need to exit the command early.
+	protected boolean exitEarly() {
+		return false;
+	}
+	
+	// Decides which distance to drive.
+	protected double getDistanceToDrive() {
+		return distanceToDriveRight;
+	}
+}
