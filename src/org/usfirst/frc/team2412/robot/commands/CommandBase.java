ea02578eac10c14b01e41a86b6429801b5112fb2
s-seibeli <s-seibeli@52S500036765.it.bsd405.org>
2018-01-27 13:09:54 -0800

    YOU OLD ME TO DO THIS

--- src/org/usfirst/frc/team2412/robot/commands/CommandBase.java
+++ src/org/usfirst/frc/team2412/robot/commands/CommandBase.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc.team2412.robot.commands;
 
 import org.usfirst.frc.team2412.robot.subsystems.DriveBaseSubsystem;
+import org.usfirst.frc.team2412.robot.subsystems.LiftSubsystem;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class CommandBase extends Command {
 
 	public static DriveBaseSubsystem driveBase = new DriveBaseSubsystem();
+	public static LiftSubsystem lift = new LiftSubsystem();
 	
 	@Override
 	protected boolean isFinished() {
