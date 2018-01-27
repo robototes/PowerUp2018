1eefdf2ee65d8acd8a73704a196a4d6fcec03e85
s-seibeli <s-seibeli@52S500036765.it.bsd405.org>
2018-01-27 12:54:25 -0800

    Simple Program Test

---
+++ src/org/usfirst/frc/team2412/robot/subsystems/LiftSubsystem.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team2412.robot.subsystems;
+
+import org.usfirst.frc.team2412.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class LiftSubsystem extends Subsystem{
+	private WPI_TalonSRX LiftMotor1 = RobotMap.liftmoter1;
+	private WPI_TalonSRX LiftMotor2 = RobotMap.liftmoter2;
+	public void LiftUpwards
+	public void LiftDownwads 
+	public void liftup() {
+		liftmotor1.set(1.0);	
+		liftmotor2.set(1.0);
+	}
+	public void liftdown() {
+		liftmotor1.set(-1.0);	
+		liftmotor2.set(-1.0);
+	}
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	} 
+
+}
