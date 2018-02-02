0f73e50e8d3b862ed54a0a61b451c4514fb1e2b6
Robbie Robototes <robototes2412@gmail.com>
2018-02-01 18:14:26 -0800

    Wait the specified number of seconds before driving.

--- src/org/usfirst/frc/team2412/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/AutonomousCommand.java
@@ -1,9 +1,12 @@
 package org.usfirst.frc.team2412.robot.commands;
 
+import org.usfirst.frc.team2412.robot.PlateColorChecker;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutonomousCommand extends CommandGroup {
 	public AutonomousCommand() {
+		addSequential(new WaitCommand(PlateColorChecker.getTimeToWait()));
 		addSequential(new DriveXFeetCommand(35000, 20000, 35000));
 		addSequential(new WaitCommand(1));
 		addSequential(new TurnCommand());
