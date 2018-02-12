89958292592deb84bf4b094512d6c596af998847
Robbie Robototes <robototes2412@gmail.com>
2018-02-12 15:54:42 -0800

    Climb up, not down.

--- src/org/usfirst/frc/team2412/robot/subsystems/ClimberSubsystem.java
+++ src/org/usfirst/frc/team2412/robot/subsystems/ClimberSubsystem.java
@@ -1,9 +1,13 @@
 package org.usfirst.frc.team2412.robot.subsystems;
 
+import org.usfirst.frc.team2412.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class ClimberSubsystem extends Subsystem {
-//private WPI_TalonSRX[] climbermotors = RobotMap.climberstalons;
+private WPI_TalonSRX[] climbermotors = RobotMap.climberstalons;
 	
 
  @Override
@@ -11,14 +15,12 @@ public class ClimberSubsystem extends Subsystem {
 		// TODO Auto-generated method stub
 
 	}
-/*
 public void climb() {
-	climbermotors[0].set(1);
-    climbermotors[1].set(1);
+	climbermotors[0].set(-1);
+    climbermotors[1].set(-1);
 }
 public void stopClimb() {
 	climbermotors[0].set(0);
 	climbermotors[1].set(0);
 }
-*/
 }
