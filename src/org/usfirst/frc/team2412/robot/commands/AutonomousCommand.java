b79c4fdd66ee3ec280f706d4a0c6ae802111f913
Robbie Robototes <robototes2412@gmail.com>
2018-02-15 20:08:23 -0800

    Drive forward slightly less in Stage 3.

--- src/org/usfirst/frc/team2412/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/AutonomousCommand.java
@@ -10,7 +10,7 @@ public class AutonomousCommand extends CommandGroup {
 		addSequential(new DriveXFeetCommand(230000, 120000, 190000));
 		addSequential(new WaitCommand(1));
 		addSequential(new TurnCommand());
-		addSequential(new DriveStage3Command(20000, 20000, 20000, -90));
+		addSequential(new DriveStage3Command(14000, 14000, 14000, -90));
 //		addSequential(new DriveLimitSwitchCommand());
 		addSequential(new DropCubeCommand());
 	}
