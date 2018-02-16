7249006ac6f8a1813d7e498301d0d3b2ec8ceb6d
Robbie Robototes <robototes2412@gmail.com>
2018-02-15 19:35:35 -0800

    Drive forward the right distance in center autonomous.

--- src/org/usfirst/frc/team2412/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/AutonomousCommand.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousCommand extends CommandGroup {
 	public AutonomousCommand() {
 		addSequential(new WaitCommand(PlateColorChecker.getTimeToWait()));
-		addSequential(new DriveXFeetCommand(230000, 140000, 230000));
+		addSequential(new DriveXFeetCommand(230000, 120000, 230000));
 		addSequential(new WaitCommand(1));
 		addSequential(new TurnCommand());
 		addSequential(new DriveStage3Command(20000, 20000, 20000, -90));
