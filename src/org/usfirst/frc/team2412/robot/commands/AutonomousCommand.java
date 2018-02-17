0329cf1dba1c6dd7e6a93332175bd381bd2da649
Robbie Robototes <robototes2412@gmail.com>
2018-02-17 15:45:34 -0800

    Bring intake arms up.

--- src/org/usfirst/frc/team2412/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/AutonomousCommand.java
@@ -13,7 +13,7 @@ public class AutonomousCommand extends CommandGroup {
 		addSequential(new IntakeCubeAutoCommand());
 		addSequential(new EncoderCommand(-0.5, 2000, 2000, 2000));
 		addSequential(new AutoIntakeInCommand());
-		
+		addSequential(new IntakeUp());
 		/*
 		addSequential(new WaitCommand(PlateColorChecker.getTimeToWait()));
 		addSequential(new DriveXFeetCommand(0.5, 230000, 120000, 190000));
