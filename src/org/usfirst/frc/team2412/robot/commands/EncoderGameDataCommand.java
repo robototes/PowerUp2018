0cd390f7748ab8a9898f00d222f7eca8fdde3cba
Robbie Robototes <robototes2412@gmail.com>
2018-02-20 15:57:38 -0800

    Added EncoderGameDataCommand with constructor.

---
+++ src/org/usfirst/frc/team2412/robot/commands/EncoderGameDataCommand.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team2412.robot.commands;
+
+public class EncoderGameDataCommand extends EncoderCommand {
+
+	public EncoderGameDataCommand(double speed, double leftDistance, double rightDistance) {
+		super(speed, 0);
+	}
+}
