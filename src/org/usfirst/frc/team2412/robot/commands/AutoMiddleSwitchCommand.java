89ef62457bf0268e933586a71cadc197d5d139c9
Robbie Robototes <robototes2412@gmail.com>
2018-02-20 20:25:46 -0800

    Fixed turn directions.

--- src/org/usfirst/frc/team2412/robot/commands/AutoMiddleSwitchCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/AutoMiddleSwitchCommand.java
@@ -4,18 +4,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutoMiddleSwitchCommand extends CommandGroup {
 	public AutoMiddleSwitchCommand() {
-		addSequential(new TurnGameDataCommand(-90));
-		/*
 		addSequential(new ShiftLowGearCommand());
 		addSequential(new EncoderCommand(-0.5, 20000));
 		addSequential(new WaitCommand(1));
-		addSequential(new TurnGameDataCommand(90));
-		addSequential(new EncoderCommand(-0.5, 30000, 90));
+		addSequential(new TurnGameDataCommand(-90));
+		addSequential(new EncoderCommand(-0.5, 30000, -90));
 //		addSequential(new EncoderCommand(-0.5, 70000, 30000, 30000, 90));
 		addSequential(new WaitCommand(1));
 		addSequential(new TurnGameDataCommand(0));
 		addSequential(new EncoderCommand(-0.5, 110000));
 		addSequential(new DropCubeCommand());
-		*/
 	}
 }
