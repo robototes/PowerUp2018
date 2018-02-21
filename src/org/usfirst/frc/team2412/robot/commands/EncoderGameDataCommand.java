703718984932f30647851a1198528149e9b00e88
Robbie Robototes <robototes2412@gmail.com>
2018-02-20 16:01:41 -0800

    Added leftDistance and rightDistance member variables.

--- src/org/usfirst/frc/team2412/robot/commands/EncoderGameDataCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/EncoderGameDataCommand.java
@@ -2,7 +2,11 @@ package org.usfirst.frc.team2412.robot.commands;
 
 public class EncoderGameDataCommand extends EncoderCommand {
 
+	private double distanceToDriveLeft;
+	private double distanceToDriveRight;
 	public EncoderGameDataCommand(double speed, double leftDistance, double rightDistance) {
 		super(speed, 0);
+		distanceToDriveLeft = leftDistance;
+		distanceToDriveRight = rightDistance;
 	}
 }
