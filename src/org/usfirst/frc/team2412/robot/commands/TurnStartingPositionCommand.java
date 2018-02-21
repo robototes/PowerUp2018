14f9657c2f593afff1a998c93134cc735df0a07a
Robbie Robototes <robototes2412@gmail.com>
2018-02-20 19:38:39 -0800

    Added constructor to set angle for TurnStartingPositionCommand.

--- src/org/usfirst/frc/team2412/robot/commands/TurnStartingPositionCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/TurnStartingPositionCommand.java
@@ -3,6 +3,10 @@ package org.usfirst.frc.team2412.robot.commands;
 import org.usfirst.frc.team2412.robot.PlateColorChecker;
 
 public class TurnStartingPositionCommand extends TurnCommand {
+	public TurnStartingPositionCommand(int angle) {
+		super(angle);
+	}
+	
 	protected double getAngleToTurn() {
 		if(PlateColorChecker.getStartingPosition().equals("Left")) {
 			return -super.getAngleToTurn();
