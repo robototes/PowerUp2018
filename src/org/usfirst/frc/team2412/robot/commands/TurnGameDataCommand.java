3923761547d560ccb4b8d65e26e81de0351cbfc0
Robbie Robototes <robototes2412@gmail.com>
2018-02-20 19:37:29 -0800

    Added constructor to set angle for TurnGameDataCommand.

--- src/org/usfirst/frc/team2412/robot/commands/TurnGameDataCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/TurnGameDataCommand.java
@@ -3,6 +3,10 @@ package org.usfirst.frc.team2412.robot.commands;
 import org.usfirst.frc.team2412.robot.PlateColorChecker;
 
 public class TurnGameDataCommand extends TurnCommand {
+	public TurnGameDataCommand(int angle) {
+		super(angle);
+	}
+
 	protected double getAngleToTurn() {
 		if(PlateColorChecker.isLeftSwitchCorrectColor()) {
 			return -super.getAngleToTurn();
