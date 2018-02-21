46dfcee8af1ac7919c28014757b2ef240da5f67b
Robbie Robototes <robototes2412@gmail.com>
2018-02-20 16:07:55 -0800

    Added EncoderGameDataCommand constructor with angle.

--- src/org/usfirst/frc/team2412/robot/commands/EncoderGameDataCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/EncoderGameDataCommand.java
@@ -4,8 +4,15 @@ public class EncoderGameDataCommand extends EncoderCommand {
 
 	private double distanceToDriveLeft;
 	private double distanceToDriveRight;
+	
 	public EncoderGameDataCommand(double speed, double leftDistance, double rightDistance) {
 		super(speed, 0);
+		distanceToDriveLeft = leftDistance;
+		distanceToDriveRight = rightDistance;
+	}
+	
+	public EncoderGameDataCommand(double speed, double leftDistance, double rightDistance, double angle) {
+		super(speed, 0, angle);
 		distanceToDriveLeft = leftDistance;
 		distanceToDriveRight = rightDistance;
 	}
