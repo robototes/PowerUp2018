55769e33c3d8d0aa166ede241ec41834c46b0a5b
Timothy Eng <timothyeng36@gmail.com>
2018-02-23 15:56:44 -0800

    Split AutoScaleCommand into left and right components.

---
+++ src/org/usfirst/frc/team2412/robot/commands/AutoRightScaleCommand.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team2412.robot.commands;
+
+import org.usfirst.frc.team2412.robot.PlateColorChecker;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutoRightScaleCommand extends CommandGroup {
+	public AutoRightScaleCommand() {
+		addSequential(new ShiftHighGearCommand());
+		addSequential(new WaitCommand(PlateColorChecker.getTimeToWait()));
+		addSequential(new EncoderCommand(0.8, 460000, 2));
+		addSequential(new ShiftLowGearCommand());
+		addSequential(new WaitCommand(1));
+		addSequential(new TurnCommand(80));
+		addSequential(new IntakeDown()); //TODO COMMENT OUT THIS LINE WHEN WE'RE AT SAMMAMISH
+		addSequential(new LiftUpLimitSwitchCommand());
+		addSequential(new EncoderCommand(0.2, 20000, 90));
+		addSequential(new DropCubeCommand());
+	}
+}
