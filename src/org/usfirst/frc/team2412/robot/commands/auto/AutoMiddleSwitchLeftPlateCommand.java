0f95576970b1f01b73a5d3fb457732bf66c70acc
Timothy Eng <timothyeng36@gmail.com>
2018-02-23 16:03:46 -0800

    Don't use TurnGameDataCommand in middle switch auto routines.

--- src/org/usfirst/frc/team2412/robot/commands/auto/AutoMiddleSwitchLeftPlateCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/auto/AutoMiddleSwitchLeftPlateCommand.java
@@ -4,7 +4,6 @@ import org.usfirst.frc.team2412.robot.commands.DropCubeCommand;
 import org.usfirst.frc.team2412.robot.commands.EncoderCommand;
 import org.usfirst.frc.team2412.robot.commands.ShiftLowGearCommand;
 import org.usfirst.frc.team2412.robot.commands.TurnCommand;
-import org.usfirst.frc.team2412.robot.commands.TurnGameDataCommand;
 import org.usfirst.frc.team2412.robot.commands.WaitCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -19,7 +18,7 @@ public class AutoMiddleSwitchLeftPlateCommand extends CommandGroup {
 //		addSequential(new EncoderCommand(-0.5, 70000, 30000, 30000, 90));
 		
 		addSequential(new WaitCommand(1));
-		addSequential(new TurnGameDataCommand(0));
+		addSequential(new TurnCommand(0));
 		addSequential(new EncoderCommand(-0.5, 110000));
 		addSequential(new DropCubeCommand());
 	}
