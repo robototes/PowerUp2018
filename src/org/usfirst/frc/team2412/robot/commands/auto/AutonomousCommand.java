1e545091b3c0b5e11fea1b459cf70c250be1fd78
Robbie Robototes <robototes2412@gmail.com>
2018-03-12 07:52:28 -0700

    Use FollowTrajectoryCommand.

--- src/org/usfirst/frc/team2412/robot/commands/auto/AutonomousCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/auto/AutonomousCommand.java
@@ -1,15 +1,14 @@
 package org.usfirst.frc.team2412.robot.commands.auto;
 
-import org.usfirst.frc.team2412.robot.PlateColorChecker;
-import org.usfirst.frc.team2412.robot.commands.EncoderCommand;
-import org.usfirst.frc.team2412.robot.commands.WaitCommand;
+import org.usfirst.frc.team2412.robot.commands.FollowTrajectoryCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutonomousCommand extends CommandGroup {
 	public AutonomousCommand() {
-		//addSequential(new FollowTrajectoryCommand("Drive Forward 10 Feet", 4));
+		addSequential(new FollowTrajectoryCommand("Drive Forward 10 Feet", 4));
 		
+		/*
 		addSequential(new WaitCommand(PlateColorChecker.getTimeToWait()));
 		if(PlateColorChecker.getStartingPosition().equals("Left")) {
 			if(PlateColorChecker.isLeftSwitchCorrectColor()) {
