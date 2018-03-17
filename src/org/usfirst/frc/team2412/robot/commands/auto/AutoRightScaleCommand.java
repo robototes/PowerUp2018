9f3f90499d59289bce8554f33e5dadf8072dde25
Robbie Robototes <robototes2412@gmail.com>
2018-03-16 19:26:49 -0700

    Drive forward slower in switch autonomous.

--- src/org/usfirst/frc/team2412/robot/commands/auto/AutoRightScaleCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/auto/AutoRightScaleCommand.java
@@ -2,8 +2,6 @@ package org.usfirst.frc.team2412.robot.commands.auto;
 
 import org.usfirst.frc.team2412.robot.commands.EncoderCommand;
 import org.usfirst.frc.team2412.robot.commands.IntakeDown;
-import org.usfirst.frc.team2412.robot.commands.ShiftHighGearCommand;
-import org.usfirst.frc.team2412.robot.commands.ShiftLowGearCommand;
 import org.usfirst.frc.team2412.robot.commands.TurnCommand;
 import org.usfirst.frc.team2412.robot.commands.WaitCommand;
 
@@ -11,9 +9,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutoRightScaleCommand extends CommandGroup {
 	public AutoRightScaleCommand() {
-		addSequential(new ShiftHighGearCommand());
-		addSequential(new EncoderCommand(-0.8, 460000, 2));
-		addSequential(new ShiftLowGearCommand());
+//		addSequential(new ShiftHighGearCommand());
+		addSequential(new EncoderCommand(-0.5, 460000, 2));
+//		addSequential(new ShiftLowGearCommand());
 		addSequential(new WaitCommand(1));
 		addSequential(new TurnCommand(80));
 		addSequential(new IntakeDown()); //TODO COMMENT OUT THIS LINE WHEN WE'RE AT SAMMAMISH
