1150689bbe8916120ee0f7213b13f9b9926b5ddd
Robbie Robototes <robototes2412@gmail.com>
2018-03-16 17:54:24 -0700

    Just drive forward in NoCubeCommand.

--- src/org/usfirst/frc/team2412/robot/commands/auto/AutoRightNoCubeCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/auto/AutoRightNoCubeCommand.java
@@ -1,16 +1,14 @@
 package org.usfirst.frc.team2412.robot.commands.auto;
 
 import org.usfirst.frc.team2412.robot.commands.EncoderCommand;
-import org.usfirst.frc.team2412.robot.commands.TurnCommand;
-import org.usfirst.frc.team2412.robot.commands.WaitCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutoRightNoCubeCommand extends CommandGroup {
 	public AutoRightNoCubeCommand() {
-		addSequential(new EncoderCommand(0.5, 200000)); //TODO check distances.
-		addSequential(new WaitCommand(1));
-		addSequential(new TurnCommand(90));
-		addSequential(new EncoderCommand(0.5, 200000, 90));
+		addSequential(new EncoderCommand(0.5, 120000)); //TODO check distances.
+//		addSequential(new WaitCommand(1));
+//		addSequential(new TurnCommand(90));
+//		addSequential(new EncoderCommand(0.5, 200000, 90));
 	}
 }
