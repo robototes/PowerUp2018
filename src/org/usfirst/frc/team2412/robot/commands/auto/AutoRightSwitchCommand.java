990b73d5486477129110dfdd7367e8c6a2394876
Robbie Robototes <robototes2412@gmail.com>
2018-03-24 20:07:04 -0700

    Updated right switch autonomous.

--- src/org/usfirst/frc/team2412/robot/commands/auto/AutoRightSwitchCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/auto/AutoRightSwitchCommand.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team2412.robot.commands.auto;
 
 import org.usfirst.frc.team2412.robot.commands.EncoderCommand;
-import org.usfirst.frc.team2412.robot.commands.TimedEncoderCommand;
 import org.usfirst.frc.team2412.robot.commands.TurnCommand;
 import org.usfirst.frc.team2412.robot.commands.WaitCommand;
 
@@ -11,8 +10,9 @@ public class AutoRightSwitchCommand extends CommandGroup {
 	public AutoRightSwitchCommand() {
 		addSequential(new EncoderCommand(-0.7, 200000)); //Right-center is 120000
 		addSequential(new WaitCommand(1));
-		addSequential(new TurnCommand(-90));
-		addSequential(new TimedEncoderCommand(-0.7, 80000, -90, 2, 5E9));
+		addSequential(new TurnCommand(-90, 1, 0.015));
+		addSequential(new EncoderCommand(-0.7, 50000, -90));
+//		addSequential(new TimedEncoderCommand(-0.7, 80000, -90, 2, 1E9));
 		//addSequential(new DriveLimitSwitchCommand());
 		addSequential(new DropCubeCommand());
 	}
