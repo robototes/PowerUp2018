d33fcd39ee61c2cce753ad7b3c477d6b5c40cd85
s-engt <s-engt@52S50015667.it.bsd405.org>
2018-05-01 17:34:15 -0700

    Added (empty) DriveForwardCommand for driving based on PID.

---
+++ src/org/usfirst/frc/team2412/robot/commands/DriveForwardCommand.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team2412.robot.commands;
+
+public class DriveForwardCommand extends CommandBase {
+
+	public DriveForwardCommand(double driveDistance, double angle, double Tp) {
+	}
+}
