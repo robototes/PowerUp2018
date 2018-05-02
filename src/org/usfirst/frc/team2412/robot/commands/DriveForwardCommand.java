afa0922d2cee60b5fa4bb3c47961739af314e4a6
s-engt <s-engt@52S50015667.it.bsd405.org>
2018-05-01 18:19:25 -0700

    Added DriveForwardCommand constructors.

--- src/org/usfirst/frc/team2412/robot/commands/DriveForwardCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/DriveForwardCommand.java
@@ -1,7 +1,26 @@
 package org.usfirst.frc.team2412.robot.commands;
 
 public class DriveForwardCommand extends CommandBase {
-
+	// Distance to drive (encoder units).
+	private double distanceToDrive = 0;
+	
+	// Heading to maintain while driving (degrees).
+	private double angleToTurn = 0;
+	
+	// P for turning.
+	private double Tp = 2;
+	
+	public DriveForwardCommand(double driveDistance) {
+		this(driveDistance, 0, 2);
+	}
+	
+	public DriveForwardCommand(double driveDistance, double angle) {
+		this(driveDistance, angle, 2);
+	}
+	
 	public DriveForwardCommand(double driveDistance, double angle, double Tp) {
+		this.distanceToDrive = driveDistance;
+		this.angleToTurn = angle;
+		this.Tp = Tp;
 	}
 }
