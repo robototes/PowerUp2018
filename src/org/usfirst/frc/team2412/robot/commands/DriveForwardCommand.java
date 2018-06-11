24fd1520a00a8b8f5f95c62a3289b386ab1e32cf
Robototes Programmers <programmers@robototes.com>
2018-06-11 16:50:05 -0700

    Retuned PID controller and set an absolute tolerance.

--- src/org/usfirst/frc/team2412/robot/commands/DriveForwardCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/DriveForwardCommand.java
@@ -30,7 +30,7 @@ public class DriveForwardCommand extends CommandBase {
 	protected void initialize() {
 		driveBase.setInputRange(0, distanceToDrive);
 		driveBase.setSetpoint(distanceToDrive);
-		driveBase.setPercentTolerance(1);
+		driveBase.setAbsoluteTolerance(2500);
 		driveBase.getPIDController().enable();
 		driveBase.setTurnAngle(angleToTurn);
 		driveBase.resetLeftEncoder();
