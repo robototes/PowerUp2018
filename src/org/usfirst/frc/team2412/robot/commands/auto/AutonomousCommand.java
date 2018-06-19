9e1b17c99ebd7fa3f3ba481ebb68688aa9c2bbd3
Robototes Programmers <programmers@robototes.com>
2018-06-18 17:03:25 -0700

    Redid middle switch left plate autonomous.

--- src/org/usfirst/frc/team2412/robot/commands/auto/AutonomousCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/auto/AutonomousCommand.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team2412.robot.commands.auto;
 
 import org.usfirst.frc.team2412.robot.commands.DriveForwardCommand;
 import org.usfirst.frc.team2412.robot.commands.TurnCommand;
+import org.usfirst.frc.team2412.robot.commands.WaitCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -9,11 +10,12 @@ public class AutonomousCommand extends CommandGroup {
 	public AutonomousCommand() {
 //		addSequential(new DriveForwardCommand(180000, 0, 2, 0.0000045, 0.000000083, 0.0000001));
 		
-		addSequential(new DriveForwardCommand(90000, 0, 2, 0.0000045, 0.000000083, 0.0000001));
+		addSequential(new DriveForwardCommand(85000, 0, 10, 0.0000045, 0.000000083, 0.0000001));
 		addSequential(new TurnCommand(-90, 1.0, 0.01));
-		addSequential(new DriveForwardCommand(180000, 0, 2, 0.0000045, 0.000000083, 0.0000001));
+		addSequential(new DriveForwardCommand(145000, -90, 10, 0.0000045, 0.000000083, 0.0000001));
+		addSequential(new WaitCommand(1));
 		addSequential(new TurnCommand(0, 1.0, 0.01));
-		addSequential(new DriveForwardCommand(90000, 0, 2, 0.0000045, 0.000000083, 0.0000001));
+		addSequential(new DriveForwardCommand(70000, 0, 2, 0.00001, 0.00000005, 0.0000001));
 		
 //		addSequential(new WaitCommand(1));
 //		addSequential(new TurnCommand(90, 1.0, 0.01));
