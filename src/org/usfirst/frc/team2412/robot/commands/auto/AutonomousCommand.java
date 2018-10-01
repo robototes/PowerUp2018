febb5444f025c61dfb0b234782d2b855d80a1d41
Robototes Programmers <programmers@robototes.com>
2018-10-01 16:08:35 -0700

    Made middle switch left plate faster.

--- src/org/usfirst/frc/team2412/robot/commands/auto/AutonomousCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/auto/AutonomousCommand.java
@@ -9,12 +9,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousCommand extends CommandGroup {
 	public AutonomousCommand() {
 		addSequential(new DriveForwardCommand(30000, 0, 10, 0.00002, 0.000001, 0.0000001));
-		addSequential(new WaitCommand(0.5));
-		addSequential(new TurnCommand(-60, 0.9, 0.01));
+		addSequential(new WaitCommand(0.3));
+		addSequential(new TurnCommand(-60, 1.0, 0.01));
 		addSequential(new DriveForwardCommand(220000, -60, 10, 0.000002, 0.00000005, 0.0000001));
-		addSequential(new WaitCommand(0.5));
+		addSequential(new WaitCommand(0.3));
 		addSequential(new TurnCommand(0, 0.9, 0.01));
-		addSequential(new WaitCommand(0.5));
+		addSequential(new WaitCommand(0.3));
 		addSequential(new DriveForwardCommand(27000, 0, 10, 0.00005, 0.000000, 0.0000001));
 		addSequential(new DropCubeCommand());
 		/**
